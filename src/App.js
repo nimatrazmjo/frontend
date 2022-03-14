3faac2a1f145e388141c33db5e3f93e9ecd17edc
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-03-14 09:47:13 +0430

    Add dummy text in feature branch

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, Hell no
-        </a>
+        <h1> Hello from react</h1>
       </header>
+      <section>
+        <p>
+          And raven nothing for nodded of, hauntedtell still midnight shall this then with, yore its ungainly the fiery caught. The a did that soul did farther door the, expressing get the more press i. For before of what engaged quoth truly fowl, never still be sorrowsorrow and his, lamplight borrow muttered my now more. The seeing i lining my ominous marvelled lordly. Startled or ominous back horror not memories into lies nothing, nothing swung is of lost. But but door lamplight what hesitating for broken on, floor of if visiter raven stillness plutonian and the. Have weak i ah visiter.
+        </p>
+      </section>
     </div>
   );
 }
